d431a88525774ecf46f3e31626620adae3b625bd
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2020-11-10 00:01:06 +0100

    .

--- lib/song_raw.dart
+++ lib/song_raw.dart
@@ -240,24 +240,27 @@ class SongRaw implements SongCore{
     int refCount = 0;
     for (SongPart part in songParts) {
 
-      if (hasRefren && part.element == refrenPart?.element) { //part.isRefren
-        refCount++;
-        continue;
-      }
+      if(part.element == refrenPart?.element)
+        if (hasRefren) { //part.isRefren
+          refCount++;
+          continue;
+        }
 
-      if(hasRefren && refCount>0) {
-        parts.add({'refren': refCount});
-        refCount = 0;
-      }
+      else {
 
-      if(part.element != refrenPart?.element){
-        parts.add({
-          'text': part.getText(),
-          'chords': part.chords,
-          'shift': part.shift
-        });
-      }
+          if (hasRefren && refCount > 0) {
+            parts.add({'refren': refCount});
+            refCount = 0;
+          }
 
+          if (part.element != refrenPart?.element) {
+            parts.add({
+              'text': part.getText(),
+              'chords': part.chords,
+              'shift': part.shift
+            });
+          }
+        }
     }
 
     if(hasRefren && refCount>0)
